cfa0b33bcf9a6d8c7684d6eb2d6bec1c6c8ecf29
KarVarr <megatr9n@gmail.com>
2022-12-03 21:55:49 +0300

    Initial Commit

---
+++ Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Karen Vardanian on 03.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

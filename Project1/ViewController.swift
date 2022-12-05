f724f7ac792d4e92ce175c66f77c4679a21616e1
KarVarr <megatr9n@gmail.com>
2022-12-05 21:36:03 +0300

    Challenge done! Sort name of the picture Changed the size of title make a naming for the pictures. Picture X of Y

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -19,7 +19,8 @@ class ViewController: UITableViewController {
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
+        let items = try! fm.contentsOfDirectory(atPath: path).sorted()
+        
         
         for item in items {
             if item.hasPrefix("nssl") {
@@ -27,22 +28,29 @@ class ViewController: UITableViewController {
             }
         }
         
-        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return pictures.count
     }
+//    pictures[indexPath.row]
+    
+    
+    
+   
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.text = "Picture \(pictures.firstIndex(of: pictures[indexPath.row])! + 1) of \(pictures.count)"
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
             vc.selectedImage = pictures[indexPath.row]
+            
             navigationController?.pushViewController(vc, animated: true)
         }
     }
